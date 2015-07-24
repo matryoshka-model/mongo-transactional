ac596d21df7bb02c64bfc5d9675319785260d415
Leonardo Grasso <me@leonardograsso.com>
2015-07-24 11:17:15 +0200

    Initial setup

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * MongoDB Transaction
+ *
+ * @link        https://github.com/matryoshka-model/mongo-transaction
+ * @copyright   Copyright (c) 2015, Ripa Club
+ * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+chdir(__DIR__);
+
+if (!file_exists('../vendor/autoload.php')) {
+    throw new \RuntimeException('vendor/autoload.php not found. Run a composer install.');
+}
