76372e84402a66397d9394580ff385ba3390a9ce
Leonardo Di Donato <leodidonato@gmail.com>
2015-07-24 16:24:10 +0200

    ErrorTrait test completed

--- tests/Error/ErrorTraitTest.php
+++ tests/Error/ErrorTraitTest.php
@@ -4,6 +4,8 @@ namespace MatryoshkaMongoTransactionalTest\Error;
 
 use Matryoshka\MongoTransactional\Error\ErrorTrait;
 use MatryoshkaMongoTransactionalTest\Error\TestAsset;
+use Zend\Stdlib\ArrayObject;
+use Matryoshka\MongoTransactional\Exception\InvalidArgumentException;
 
 /**
  * Class ErrorTraitTest
@@ -21,6 +23,60 @@ class ErrorTraitTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->traitObject = $this->getObjectForTrait(ErrorTrait::class);
+    }
+
+    public function testExceptionClassSetterAndGetter()
+    {
+        $this->assertSame($this->traitObject, $this->traitObject->setExceptionClass(\Exception::class));
+        $this->assertSame(\Exception::class, $this->traitObject->getExceptionClass());
+        $this->assertSame($this->traitObject, $this->traitObject->setExceptionClass(false));
+        $this->assertNull($this->traitObject->getExceptionClass());
+        $this->assertSame($this->traitObject, $this->traitObject->setExceptionClass(null));
+        $this->assertNull($this->traitObject->getExceptionClass());
+    }
+
+    public function testCodeSetterAndGetter()
+    {
+        $this->assertSame($this->traitObject, $this->traitObject->setCode(1));
+        $this->assertSame(1, $this->traitObject->getCode());
+        $this->assertSame($this->traitObject, $this->traitObject->setCode('abc'));
+        $this->assertSame('abc', $this->traitObject->getCode());
+        $this->assertSame($this->traitObject, $this->traitObject->setCode(true));
+        $this->assertSame('1', $this->traitObject->getCode());
+        $this->assertSame($this->traitObject, $this->traitObject->setCode(false));
+        $this->assertNull($this->traitObject->getCode());
+        $this->assertSame($this->traitObject, $this->traitObject->setCode(null));
+        $this->assertNull($this->traitObject->getCode());
+    }
+
+    public function testMessageSetterAndGetter()
+    {
+        $this->assertSame($this->traitObject, $this->traitObject->setMessage('mex'));
+        $this->assertSame('mex', $this->traitObject->getMessage());
+        $this->assertSame($this->traitObject, $this->traitObject->setMessage(false));
+        $this->assertNull($this->traitObject->getMessage());
+        $this->assertSame($this->traitObject, $this->traitObject->setMessage(null));
+        $this->assertNull($this->traitObject->getMessage());
+    }
+
+    public function testAdditionalDetailsSetterAndGetter()
+    {
+        $testArray = [1, 2, 3];
+        $testTraversable = new ArrayObject($testArray);
+        $this->assertSame($this->traitObject, $this->traitObject->setAdditionalDetails($testTraversable));
+        $this->assertSame($testArray, $this->traitObject->getAdditionalDetails());
+
+        $this->assertSame($this->traitObject, $this->traitObject->setAdditionalDetails($testArray));
+        $this->assertSame($testArray, $this->traitObject->getAdditionalDetails());
+
+        $testNull = null;
+        $this->assertSame($this->traitObject, $this->traitObject->setAdditionalDetails($testNull));
+        $this->assertEmpty($this->traitObject->getAdditionalDetails());
+
+        $testNotArray = 123456;
+        $this->setExpectedException(InvalidArgumentException::class);
+        $this->traitObject->setAdditionalDetails($testNotArray);
+
     }
 
     public function testFromException()
